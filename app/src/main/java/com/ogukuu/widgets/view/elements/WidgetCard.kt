2ffb0f0091ad5128d4f60c05eed43ade384f31b3
ogukuu <ogukuu@gmail.com>
2022-05-23 11:35:15 +0300

    elevation WidgetCard

--- app/src/main/java/com/ogukuu/widgets/view/elements/WidgetCard.kt
+++ app/src/main/java/com/ogukuu/widgets/view/elements/WidgetCard.kt
@@ -2,10 +2,12 @@ package com.ogukuu.widgets.view.elements
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -34,7 +36,11 @@ fun WidgetCard(navHostController: NavHostController, widget: Widget, nameWidget:
             Box(
                 modifier = Modifier
                     .padding(all = 6.dp)
-                    .requiredSize(70.dp),
+                    .requiredSize(70.dp)
+                    .shadow(
+                        elevation = 1.dp,
+                        shape = RoundedCornerShape(4.dp)
+                    ),
                 contentAlignment = Alignment.Center
             ){
                 widget.IconWidget()
